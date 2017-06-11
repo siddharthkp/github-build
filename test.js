e67cd32aa3af330d03e37b097db71457cb843108
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-11 20:00:40 +0530

    defaults for local tests

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const Build = require('./index')
 
 const data = {
-  repo: process.env.TRAVIS_REPO_SLUG,
-  sha: process.env.TRAVIS_PULL_REQUEST_SHA,
+  repo: 'siddharthkp/github-build',
+  sha: process.env.TRAVIS_PULL_REQUEST_SHA ||
+    '4391039e9c506a1702ee7971cda4613ca5da2d69',
   token: process.env.github_token,
   label: 'github-build',
   description: 'Running some tests'
