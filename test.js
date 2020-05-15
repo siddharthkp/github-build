2718df786f578f1366b9affc233249dfbb73f377
Jimmy Multani <jimmy.multani@rakuten.com>
2020-05-14 23:26:47 -0400

    look at TRAVIS_REPO_SLUG too

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const Build = require('./index')
 
 const data = {
-  repo: process.env.TRAVIS_PULL_REQUEST_SLUG || 'siddharthkp/github-build',
+  repo: process.env.TRAVIS_PULL_REQUEST_SLUG || process.env.TRAVIS_REPO_SLUG || 'siddharthkp/github-build',
   sha: process.env.TRAVIS_PULL_REQUEST_SHA ||
     '4391039e9c506a1702ee7971cda4613ca5da2d69',
   token: process.env.github_token,
@@ -15,6 +15,7 @@ setTimeout(() => build.pass('Tests passed!', 'https://example.com'), 5000)
 
 process.on('unhandledRejection', (reason, p) => {
   console.log('TRAVIS_PULL_REQUEST_SLUG', process.env.TRAVIS_PULL_REQUEST_SLUG);
+  console.log('TRAVIS_PULL_REQUEST_SHA', process.env.TRAVIS_PULL_REQUEST_SHA);
 
   console.log('Unhandled Promise: ')
   console.log(reason, p)
