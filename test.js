c36f4762e1c8f2c252bd21ae476c6f2b1569c979
Jimmy Multani <jimmy.multani@rakuten.com>
2020-05-14 23:22:52 -0400

    debug test

--- test.js
+++ test.js
@@ -14,6 +14,8 @@ build.start()
 setTimeout(() => build.pass('Tests passed!', 'https://example.com'), 5000)
 
 process.on('unhandledRejection', (reason, p) => {
+  console.log('TRAVIS_PULL_REQUEST_SLUG', process.env.TRAVIS_PULL_REQUEST_SLUG);
+
   console.log('Unhandled Promise: ')
   console.log(reason, p)
   process.exit(1)
