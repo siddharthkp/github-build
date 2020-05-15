b539688c811002b4da0ea1960b50598c4ac5bad1
Jimmy Multani <jimmy.multani@rakuten.com>
2020-05-14 23:17:01 -0400

    pass along the slug to make this work for PRs

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const Build = require('./index')
 
 const data = {
-  repo: 'siddharthkp/github-build',
+  repo: process.env.TRAVIS_PULL_REQUEST_SLUG || 'siddharthkp/github-build',
   sha: process.env.TRAVIS_PULL_REQUEST_SHA ||
     '4391039e9c506a1702ee7971cda4613ca5da2d69',
   token: process.env.github_token,
