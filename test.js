eb3f5bc055369d0c7058989d4a594d770a4689c4
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2023-11-21 17:20:47 +0100

    test: use SHA from environment

--- test.js
+++ test.js
@@ -2,8 +2,7 @@ const Build = require('./index')
 
 const data = {
   repo: 'siddharthkp/github-build',
-  sha: process.env.TRAVIS_PULL_REQUEST_SHA ||
-    '4391039e9c506a1702ee7971cda4613ca5da2d69',
+  sha: process.env.SHA || process.env.TRAVIS_PULL_REQUEST_SHA || '4391039e9c506a1702ee7971cda4613ca5da2d69',
   token: process.env.GITHUB_TOKEN,
   label: 'github-build',
   description: 'Running some tests'
